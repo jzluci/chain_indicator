06d094cd7f7e304c5a68964b83a1f5f737077f6c
jzluci <jzluci00@gmail.com>
2024-02-29 03:45:43 -0600

    working like buttons with progress bar. Needs icon to move with progress bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
 
+
 void main() {
   runApp(MyApp());
 }
@@ -20,18 +21,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isLiked1 = false;
+  bool isLiked2 = false;
+  bool isLiked3 = false;
   double progress = 0.0;
+  int likeCounter = 0;
+  bool isVisible = false;
 
-  Future<bool> updateProgress(bool isLiked) async{
-    setState(() {
-      progress += 0.25;
-      if (progress > 1.0) {
-        progress = 0.0;
-      }
-    }
-    );
-    return !isLiked;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,32 +36,97 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Custom Progress Indicator'),
       ),
       body: Center(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(left:2.0),
-                child: ChainLinkProgressIndicator(progress: progress),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RotatedBox(//progress bar
+                  quarterTurns: 3,
+                  child: LinearProgressIndicator(
+                    value: progress,
+                  )
               ),
             ),
-            SizedBox(height: 20),
-            //ElevatedButton(onPressed: updateProgress, child: Text('Click Me')),
             LikeButton(
-              size: 80,
-              onTap: updateProgress,
-              bubblesColor:BubblesColor(
-                dotPrimaryColor: Colors.blueAccent,
-                dotSecondaryColor: Colors.lightBlueAccent,
-              ),
-              likeBuilder: (bool isLiked) {
-                return Icon(
-                  Icons.favorite,
-                  color: Colors.red,
-                  size: 80,
-                );
+              size: 30,
+              isLiked: isLiked1,
+              likeBuilder: (isLiked){
+                final color = isLiked1 ? Colors.blue : Colors.grey;
+                return Icon(Icons.spa, color: color, size: 30,);
               },
+              onTap: (isLiked) async{
+                isLiked1 = !isLiked1;
+                progress += isLiked1 ? .333333 : -.333333;
+                likeCounter += isLiked1 ? 1 : -1;
+                if(likeCounter != 0){
+                  isVisible = true;
+                }
+                else{
+                  isVisible = false;
+                }
+
+                Future.delayed(Duration(milliseconds: 10)).then((value) => setState(() {}));
+
+                return !isLiked;
+              },
+            ),
+            LikeButton(
+              size: 30,
+              isLiked: isLiked2,
+              likeBuilder: (isLiked){
+                final color = isLiked2 ? Colors.blue : Colors.grey;
+                return Icon(Icons.spa, color: color, size: 30,);
+              },
+              onTap: (isLiked) async{
+                isLiked2 = !isLiked2;
+                progress += isLiked2 ? .333333 : -.333333;
+                likeCounter += isLiked2 ? 1 : -1;
+                if(likeCounter != 0){
+                  isVisible = true;
+                }
+                else{
+                  isVisible = false;
+                }
+
+                Future.delayed(Duration(milliseconds: 10)).then((value) => setState(() {}));
+
+                return !isLiked;
+              },
+            ),
+            LikeButton(
+              size: 30,
+              isLiked: isLiked3,
+              likeBuilder: (isLiked){
+                final color = isLiked3 ? Colors.blue : Colors.grey;
+                return Icon(Icons.spa, color: color, size: 30,);
+              },
+              onTap: (isLiked) async{
+                isLiked3 = !isLiked3;
+                progress += isLiked3 ? .333333 : -.333333;
+                likeCounter += isLiked3 ? 1 : -1;
+                if(likeCounter != 0){
+                  isVisible = true;
+                }
+                else{
+                  isVisible = false;
+                }
+
+                Future.delayed(Duration(milliseconds: 10)).then((value) => setState(() {}));
+
+                return !isLiked;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(likeCounter.toString()),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Visibility(
+                visible: isVisible,
+                child: Text("See Profile")
+              ),
             ),
           ],
         ),
@@ -73,108 +134,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-class ChainLinkProgressIndicator extends StatelessWidget {
-  final double progress;
-
-  ChainLinkProgressIndicator({required this.progress});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      child: Row(
-        children: [
-          for (int i = 0; i < 8; i++)
-            Row(
-              children: [
-                ChainLink(
-                  filled: i < (progress * 8).floor(),
-                ),
-                OvalSpacer(
-                  filled: i < (progress * 8).floor(),
-                  visible: i < 7,
-
-                )
-              ],
-            ),
-        ],
-      ),
-    );
-  }
-}
-
-class OvalSpacer extends StatelessWidget {
-  final bool visible;
-  final bool filled;
-
-  OvalSpacer({required this.visible, required this.filled});
-
-  @override
-  Widget build(BuildContext context) {
-    return Visibility(
-      visible: visible,
-      child: Container(
-        width: MediaQuery.of(context).size.width / 32 ,
-        height: 10,
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 150),
-          curve: Curves.linear,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: filled ? Colors.blue : Colors.grey,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
-class ChainLink extends StatelessWidget {
-  final bool filled;
-
-  ChainLink({required this.filled});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.sizeOf(context).width / 10.4,
-      height: 20,
-      //margin: EdgeInsets.symmetric(horizontal: 5),
-      decoration: BoxDecoration(
-        color: Colors.transparent, // Make the outer part transparent
-      ),
-      child: Stack(
-        children: [
-          // Outer part with rounded corners
-          AnimatedContainer(
-            duration: Duration(milliseconds: 150),
-            curve: Curves.linear,
-            width: MediaQuery.sizeOf(context).width / 10.4,
-            height: 20,
-            decoration: BoxDecoration(
-              color: filled ? Colors.blue : Colors.grey,
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          // Inner part to make the center transparent
-          Padding(
-            padding: const EdgeInsets.all(6.0),
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 150),
-              curve: Curves.linear,
-              width: (MediaQuery.sizeOf(context).width / 10.4) - 10,
-              height: 15,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10)
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
